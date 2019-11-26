acc58a00a2a3c47a49fae2d2aea1c797345c515a
A Min <nuomi29@gmail.com>
2019-11-26 11:52:39 +0800

    copy paste codepen / import bulma & js

--- app/frontend/packs/app.js
+++ app/frontend/packs/app.js
@@ -1,72 +1,52 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
-// to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-
-import Vue from 'vue'
-import App from '../app.vue'
+import Vue from 'vue/dist/vue.esm.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
-
-  console.log(app)
+  new Vue({
+    el: '#app',
+    data: {
+      expand_type: '-',
+      title: '',
+      amount: '',
+      description: '',
+      items: [
+        {id: 1, expand_type: '-', title: '買手機', amount: 20000, description: "Hello"},
+        {id: 2, expand_type: '-', title: '買書', amount: 1000, description: "World"}
+      ]
+    },
+    methods: {
+      clear() {
+        this.expand_type = '-'
+        this.title = ''
+        this.amount = ''
+        this.description = ''
+      },
+      addItem(evt) {
+        evt.preventDefault()
+        let cc = {
+          id: 3, 
+          expand_type: this.expand_type, 
+          title: this.title, 
+          amount: this.amount, 
+          description: this.description
+        }
+        this.items.unshift(cc)
+        this.clear()
+      }
+    },
+    computed: {
+      total(){
+        return this.items.reduce(
+            function(iccu,item){
+              if(item.expand_type == '-')
+                {
+                 return iccu - item.amount
+                 }
+              else{
+                return iccu + item.amount
+              }
+              
+        },0)
+      }
+    }
+  })
 })
-
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
-
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
-//
-//
-// If the project is using turbolinks, install 'vue-turbolinks':
-//
-// yarn add vue-turbolinks
-//
-// Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
